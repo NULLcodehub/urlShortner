0915e56dbb30fa98e8273115c707e67df54cb7bb
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-06-15 18:22:39 +0600

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express=require('express')
+
+const app=express()
+
+app.use(express.json())
+
+
+
+// routess
+
+app.get('/',(req,res)=>{
+    res.json("msg: backend running")
+})
+
+
+const PORT=process.env.PORT || 5000
+app.listen(PORT,()=>console.log(`server running at ${PORT}`))
